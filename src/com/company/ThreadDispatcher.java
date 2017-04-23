e2705fe9641a3c31bbe44053acc994fb89cb1c8c
KotliarovV <vkotl@yandex.ru>
2017-04-23 21:12:44 +0500

    commit

--- src/com/company/ThreadDispatcher.java
+++ src/com/company/ThreadDispatcher.java
@@ -1,17 +1,28 @@
 package com.company;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by vitaly on 23.04.17.
  */
 public class ThreadDispatcher {
     private static ThreadDispatcher threadDispatcher;
+    private ExecutorService executorService;
+
 
     private ThreadDispatcher () {
+        executorService = Executors.newCachedThreadPool();
     }
 
     public static ThreadDispatcher instance() {
         if (threadDispatcher == null)
             threadDispatcher = new ThreadDispatcher();
         return threadDispatcher;
+    }
+
+    public void addTask (IWorker task) {
+        executorService.submit(task);
     }
 }
---
+++ src/com/company/ThreadsMonitor.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by vitaly on 23.04.17.
+ */
+public class ThreadsMonitor implements IWorker {
+
+}
