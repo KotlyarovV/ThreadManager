2ffa86e7247a55e85125920596f5833c34ae652d
KotliarovV <vkotl@yandex.ru>
2017-04-23 20:31:04 +0500

    commit

--- src/com/company/ThreadDispatcher.java
+++ src/com/company/ThreadDispatcher.java
@@ -4,14 +4,14 @@ package com.company;
  * Created by vitaly on 23.04.17.
  */
 public class ThreadDispatcher {
-    private ThreadDispatcher threadDispatcher;
+    private static ThreadDispatcher threadDispatcher;
 
     private ThreadDispatcher () {
     }
 
-    public ThreadDispatcher instance() {
-        if (this.threadDispatcher == null)
-            this.threadDispatcher = new ThreadDispatcher();
+    public static ThreadDispatcher instance() {
+        if (threadDispatcher == null)
+            threadDispatcher = new ThreadDispatcher();
         return threadDispatcher;
     }
 }
